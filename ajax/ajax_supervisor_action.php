ae6d066dc11b0bf98989743c8821ac3f3dd0d7b0
zyxbarangan <zyxbarangan@gmail.com>
2026-08-14 08:35:34 +0800

    TEST

--- ajax/ajax_supervisor_action.php
+++ ajax/ajax_supervisor_action.php
@@ -512,11 +512,11 @@ if (isset($_POST["action"]) && $_POST["action"] == "request_submission") {
     }
 
     $statusScilabPersonnel = 'Pending';
+    $initialLabPersonnelStatus = 'pending';
+    $initialCidChiefStatus = 'pending';
 
     $initialSupervisorStatus = $isFacultyOrSysadmin ? 'approved' : 'pending';
     $initialSubjectTeacherStatus = $isFacultyOrSysadmin ? 'approved' : 'pending';
-    $initialLabPersonnelStatus = $isFacultyOrSysadmin ? 'approved' : 'pending';
-    $initialCidChiefStatus = $isFacultyOrSysadmin ? 'approved' : 'pending';
 
     $stmt = $conn->prepare("INSERT INTO scilab_form_requests (
         scilabName,
