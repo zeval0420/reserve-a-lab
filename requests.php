9ddfd69cfeb6619977d1fd14bc12180dc22f202d
zyxbarangan <zyxbarangan@gmail.com>
2026-09-07 23:04:19 +0800

    minor modif

--- requests.php
+++ requests.php
@@ -976,7 +976,7 @@
                             class="custom-modal-close"
                             id="approveModalClose"
                             aria-label="Close modal">
-                        <span style="font-size: 150%">×</span>
+                        <span style="font-size: 300%">×</span>
                     </button>
 
                 </div>
@@ -1018,7 +1018,7 @@
                             class="custom-modal-close"
                             id="rejectModalClose"
                             aria-label="Close modal">
-                        <span style="font-size: 150%">×</span>
+                        <span style="font-size: 300%">×</span>
                     </button>
 
                 </div>
