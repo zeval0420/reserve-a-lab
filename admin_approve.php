509537986989cf999b5cc84f1b546c51f34a4151
zyxbarangan <zyxbarangan@gmail.com>
2026-09-11 15:05:13 +0800

    red asterisk font size

--- admin_approve.php
+++ admin_approve.php
@@ -815,6 +815,7 @@
             .required-asterisk {
                 color: #DC2626;
                 font-weight: 800;
+                font-size: 12px;
             }
         </style>
     </head>
